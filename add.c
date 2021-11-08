ee58697c1b958fa61145e4bfd9a38bb0982737f8
Shafayet Nur Alam <30376236+shafayetnuralam@users.noreply.github.com>
2021-11-08 21:43:07 +0600

    add sum
    Signed-off-by: Shafayet Nur Alam <30376236+shafayetnuralam@users.noreply.github.com>

---
+++ add.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a,b;
+    printf("Enter Number a\n");
+    scanf("%d", &a);
+    printf("Enter Number b\n");
+    scanf("%d", &b);
+
+    printf("The Sum Value %d \n", a + b);
+    return 0;
+}
